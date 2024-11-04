75b650274874064d1a396e74a5e2787f95f265a0
MAC <horlarmihleykan10@gmail.com>
2024-11-04 16:01:09 +0100

    middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import cors from "cors";
 import express, { Express, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
-// import swaggerSpec from "./config/swaggerConfig";
-// import { errorHandler, routeNotFound } from "./middleware";
+import swaggerSpec from "./config/swaggerConfig";
+import { errorHandler, routeNotFound } from "./middleware";
 
 
 
@@ -24,7 +24,7 @@ app.use(
 // app.use(Limiter);
 app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ limit: "15mb", extended: true }));
-// app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 app.get("/", (req: Request, res: Response) => {
@@ -37,11 +37,11 @@ app.get("/", (req: Request, res: Response) => {
 
 
 
-// app.use("/openapi.json", (_req: Request, res: Response) => {
-//   res.setHeader("Content-Type", "application/json");
-//   res.send(swaggerSpec);
-// });
-// app.use(errorHandler);
-// app.use(routeNotFound);
+app.use("/openapi.json", (_req: Request, res: Response) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+app.use(errorHandler);
+app.use(routeNotFound);
 
 export default app;
